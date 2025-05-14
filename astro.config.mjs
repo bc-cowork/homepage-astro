b3fdba0035b6eb01d227b4a0061bb11c11df2d3b
Manthika <tharumudu@gmail.com>
2025-05-14 16:41:58 +1200

    Implemented blog to the given design

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,15 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
+
+// export default defineConfig({
+// 	site: 'https://example.com',
+// 	integrations: [mdx(), sitemap()],
+// });
+
 export default defineConfig({
 	site: 'https://bc-cowork.github.io',
-	base: '/homepage-astro',
+	base: '/astro-content-project',
+	output: 'static',
 	integrations: [mdx(), sitemap()],
 });
