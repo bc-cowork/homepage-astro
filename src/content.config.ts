7ce6c0a6988f8f21270d32df25ff5c9d1d9bc4f3
Manthika <tharumudu@gmail.com>
2025-05-14 13:38:34 +1200

    Initial commit

---
+++ src/content.config.ts
@@ -0,0 +1,26 @@
+import { glob } from 'astro/loaders';
+import { defineCollection, z } from 'astro:content';
+
+const blog = defineCollection({
+	// Load Markdown and MDX files in the `src/content/blog/` directory.
+	loader: glob({ base: './src/content/blog', pattern: '**/*.{md,mdx}' }),
+	// Type-check frontmatter using a schema
+	schema: z.object({
+		title: z.string(),
+		description: z.string(),
+		// Transform string to Date object
+		pubDate: z.coerce.date().optional(),
+		updatedDate: z.coerce.date().optional(),
+		heroImage: z.string().optional(),
+	}),
+});
+
+const mainCollection = defineCollection({
+	type: 'data', // or 'content' if using Markdown/MDX
+	schema: z.object({
+	  title: z.string(),
+	  description: z.string().optional(),
+	}),
+  });
+
+export const collections = { blog, mainCollection };
